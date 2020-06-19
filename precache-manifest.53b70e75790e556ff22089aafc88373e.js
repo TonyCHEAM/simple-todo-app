b2a135216b9c8d78da3db6b6be2cb3d5a04548d7
Tony Cheam <tonycheam@hotmail.fr>
2020-06-19 15:34:00 +0200

    Updates

---
+++ precache-manifest.53b70e75790e556ff22089aafc88373e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920dc77f129611a932fa76c29cb2a38c",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "1746a89d27a215e925dd",
+    "url": "/simple-todo-app/static/css/main.bd66978f.chunk.css"
+  },
+  {
+    "revision": "e1c993e3070ff6d419bc",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.aadc151e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1746a89d27a215e925dd",
+    "url": "/simple-todo-app/static/js/main.bc17756e.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
